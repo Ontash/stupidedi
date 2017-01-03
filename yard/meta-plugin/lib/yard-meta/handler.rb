f5b630bad0508cb0162962c6be3e4434e1513667
Vishnu Prasanth <vishnu@ontash.net>
2017-01-03 16:34:55 +0530

    merged edited lib folder with kputnam's git repo

---
+++ yard/meta-plugin/lib/yard-meta/handler.rb
@@ -0,0 +1,114 @@
+class AbstractMethodHandler < YARD::Handlers::Ruby::Base
+  handles method_call(:abstract)
+  namespace_only
+
+  def process
+    name = statement.parameters.first.jump(:symbol, :ident).source.slice(1..-1)
+    args = statement.parameters.jump(:assoc)
+    params = []
+
+    unless args.eql?(statement.parameters)
+      if args.first.jump(:symbol, :ident).source == ":args"
+        # :args => (...)
+        case args[1].first.type
+        when :qwords_literal
+          params = args[1].first.children.map(&:source)
+        else
+          raise YARD::Parser::UndocumentableError,
+            "need to add support for #{args[1].first.type} 'abstract :args => #{args[1].first.inspect}'"
+        end
+      end
+    end
+
+    object = register(YARD::CodeObjects::MethodObject.new(namespace, name, scope)) do |o|
+      o.visibility = visibility
+      o.source     = statement.source
+      o.explicit   = false
+      o.signature  = signature(name, params)
+      o.parameters = parameters(params)
+    end
+
+    # Predicate methods return (Boolean), so automatically add the documentation
+    if name =~ /\?$/
+      if object.has_tag?(:return)
+        tag = object.tag(:return)
+        if tag.types.nil? or tag.types.empty?
+          tag.types = %w(Boolean)
+        end
+      else
+        object.docstring.add_tag(YARD::Tags::Tag.new(:return, "", "Boolean"))
+      end
+    end
+
+    # Ensure the method is marked abstract
+    unless object.has_tag?(:abstract)
+      object.docstring.add_tag(YARD::Tags::Tag.new(:abstract, ""))
+    end
+  rescue
+    $stderr.puts $!
+    $stderr.puts "\t" + $!.backtrace.join("\n\t")
+    throw $!
+  end
+
+private
+
+  def signature(name, params)
+    if params.empty?
+      "def #{name}"
+    else
+      "def #{name}(#{params.join(', ')})"
+    end
+  end
+
+  def parameters(params)
+    params.map do |p|
+      name, default = p.split("=", 2)
+      [name, default]
+    end
+  end
+end
+
+class DelegateMethodHandler < YARD::Handlers::Ruby::MethodHandler
+  handles method_call(:delegate)
+  namespace_only
+
+  def process
+    methods = statement.parameters.slice(0..-3).map{|x| x.source }
+    target  = statement.parameters.jump(:assoc)
+
+    unless target.eql?(statement.parameters)
+      if target.first.jump(:symbol, :ident).source == ":to"
+      # target = target[1].source
+
+      # if target =~ /:@/
+      #   target = "#" << target.slice(2..-1)
+      # else
+      #   target = "#" << target.slice(1..-1)
+      # end
+
+      # target = YARD::Registry.resolve(P(namespace), target, true, false)
+      # if target and target.has_tag?(:return)
+      #   target.tag(:return).types
+      # end
+      end
+    end
+
+    methods.each do |name|
+      name = name.slice(1..-1)
+
+      object = register(YARD::CodeObjects::MethodObject.new(namespace, name, scope)) do |o|
+        o.visibility = visibility
+        o.source     = statement.source
+        o.explicit   = false
+        o.group      = (scope == :instance) ?
+          "Delegated Instance Methods" :
+          "Delegated Class Methods"
+        o.namespace.groups |= [o.group]
+      end
+    end
+  rescue
+    $stderr.puts $!
+    $stderr.puts "\t" + $!.backtrace.join("\n\t")
+    throw $!
+  end
+end
---
+++ yard/rspec-plugin/lib/yard-rspec/handler.rb
@@ -0,0 +1,133 @@
+class RSpecDescribeHandler < YARD::Handlers::Ruby::Base
+  handles method_call(:describe)
+  
+  def process
+    return unless statement.last.last
+
+    describes = statement.parameters.first.jump(:string_content).source
+
+    # Remove the argument list from describe "#method(a, b, &c)"
+    if arguments = describes[/[#.](?:.+)(\([^)]*\))$/, 1]
+      describes = describes[0, describes.length - arguments.length]
+    end
+
+    unless owner.is_a?(Hash)
+      pwner = Hash[describes: describes, context: ""]
+      parse_block(statement.last.last, owner: pwner)
+    else
+      describes = owner[:describes] + describes
+      pwner = owner.merge(describes: describes)
+      parse_block(statement.last.last, owner: pwner)
+    end
+  rescue YARD::Handlers::NamespaceMissingError
+  rescue
+    $stderr.puts $!
+    $stderr.puts "\t" + $!.backtrace.join("\n\t")
+    throw $!
+  end
+end
+
+class RSpecContextHandler < YARD::Handlers::Ruby::Base
+  handles method_call(:context)
+
+  def process
+    return unless statement.last.last
+
+    if owner.is_a?(Hash)
+      context = statement.parameters.first.jump(:string_content).source
+      context = owner[:context] + context + " "
+
+      parse_block(statement.last.last, owner: owner.merge(context: context))
+    end
+  rescue YARD::Handlers::NamespaceMissingError
+  rescue
+    $stderr.puts $!
+    $stderr.puts "\t" + $!.backtrace.join("\n\t")
+    throw $!
+  end
+end
+
+class RSpecItHandler < YARD::Handlers::Ruby::Base
+  handles method_call(:it)
+  handles method_call(:its)
+  handles method_call(:specify)
+  
+  def process
+    return unless owner.is_a?(Hash)
+    return unless owner[:describes]
+
+    node = YARD::Registry.resolve(nil, owner[:describes], true)
+    spec = if statement.parameters and statement.parameters.first
+             statement.parameters.first.jump(:string_content).source
+           else
+             "untitled spec"
+           end
+
+    unless node
+      log.debug "Unknown node #{owner[:describes]} for spec defined in #{statement.file} near line #{statement.line}"
+      return
+    end
+
+    if last = statement.last.last
+      source = last.source.strip
+    else
+      source = ""
+    end
+
+    (node[:specifications] ||= []) << \
+      Hash[ name: owner[:context] + spec,
+            file: statement.file,
+            line: statement.line,
+            source: source ]
+  rescue
+    $stderr.puts $!
+    $stderr.puts "\t" + $!.backtrace.join("\n\t")
+    throw $!
+  end
+end
+
+class RSpecCheckHandler < YARD::Handlers::Ruby::Base
+  handles method_call(:check)
+
+  def process
+    return unless owner.is_a?(Hash)
+    return unless statement.first
+
+    parse_block(statement.first, owner: owner.merge(source: statement.source))
+  rescue
+    $stderr.puts $!
+    $stderr.puts "\t" + $!.backtrace.join("\n\t")
+    throw $!
+  end
+end
+
+class RSpecPropertyHandler < YARD::Handlers::Ruby::Base
+  handles method_call(:property)
+
+  def process
+    return unless owner.is_a?(Hash)
+    return unless owner[:source] and owner[:describes]
+
+    node = YARD::Registry.resolve(nil, owner[:describes], true)
+    spec = if statement.parameters
+             statement.parameters.first.jump(:string_content).source
+           else
+             "untitled property"
+           end
+
+    unless node
+      log.debug "Unknown node #{owner[:describes]} for spec defined in #{statement.file} near line #{statement.line}"
+      return
+    end
+
+    (node[:specifications] ||= []) << \
+      Hash[ name: owner[:context] + spec,
+            file: statement.file,
+            line: statement.line,
+            source: owner[:source].strip ]
+  rescue
+    $stderr.puts $!
+    $stderr.puts "\t" + $!.backtrace.join("\n\t")
+    throw $!
+  end
+end
